77d532055ab57caadce123358e8d6dc7c7326eb5
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-28 02:59:35 -0600

    Add files via upload

--- UART_capture.c
+++ UART_capture.c
@@ -10,25 +10,24 @@
 /**This is mail box to received the information from the serial port*/
 extern UART_MailBoxType UART0_MailBox;
 
-/*Flags so we can change to the next state*/
-static uint8 flag_master = FALSE;
-static uint8 flag_wrong = FALSE;
-static uint8 flag_open = FALSE;
-static uint8 flag_add = FALSE;
+/*Buffer to store the new user*/
+static sint8 user_buffer[USER_SIZER] = {FALSE};
+static sint8 psw_buffer[PSW_SIZE] = {FALSE};
 
 void capture_psw()
 {
-	sint8 psw_buffer[PSW_SIZE] = FALSE;
+	volatile uint8 index = FALSE;
+
 	if (UART0_MailBox.flag)
 	{
 		if ((ASCII_NUMBER_1 <= UART0_MailBox.mailBox) || (ASCII_NUMBER_9 >= UART0_MailBox.mailBox))
 		{
-			*psw_buffer = UART0_MailBox.mailBox;
-			UART_putChar(UART_0,*psw_buffer);
-			*psw_buffer++;
+			psw_buffer[index] = UART0_MailBox.mailBox;
+			UART_putChar(UART_0,psw_buffer[index]);
+			index++;
 			if(psw_buffer[PSW_SIZE] == NULL)
 			{
-				flag_master = TRUE;
+				flags.flag_master = TRUE;
 			}
 		}
 	}
@@ -46,18 +45,95 @@ void choose(uint8 flag_master)
 			{
 				if(ASCII_NUMBER_1 == UART0_MailBox.mailBox)
 				{
-					flag_open = TRUE;
+					flags.flag_open = TRUE;
 				}
 				if(ASCII_NUMBER_2 == UART0_MailBox.mailBox)
 				{
-					flag_add = TRUE;
+					flags.flag_add = TRUE;
 				}
 			}
 			else
 			{
-				flag_wrong = TRUE;
+				flags.flag_wrong = TRUE;
 			}
 		}
 		UART0_MailBox.flag = FALSE;
 	}
+}
+
+void add_user()
+{
+	volatile uint8 index_user = FALSE;
+	while(TRUE == flags.flag_add)
+	{
+		if (UART0_MailBox.flag)
+		{
+			user_buffer[index_user] = UART0_MailBox.mailBox;
+			if(ASCII_ENTER == UART0_MailBox.mailBox)
+			{
+				flags.flag_add = FALSE;
+				counter.users++;
+			}
+		}
+		UART0_MailBox.flag = FALSE;
+	}
+	/*Clears index to store it in the memory*/
+	index_user = FALSE;
+	switch(counter.users)
+	{
+	case BIT1:
+		for(uint8 arr_index = FALSE; arr_index < MEM_STEP; arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT2:
+		for(uint8 arr_index = MEM_STEP; arr_index < (BIT2 * MEM_STEP); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT3:
+		for(uint8 arr_index = (BIT2 * MEM_STEP); arr_index < (BIT3 * MEM_STEP); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT4:
+		for(uint8 arr_index = (BIT3 * MEM_STEP); arr_index < (BIT4 * MEM_STEP); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT5:
+		for(uint8 arr_index = (BIT4 * MEM_STEP); arr_index < (BIT5 * MEM_STEP); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	}
+}
+
+void add_code()
+{
+	if (UART0_MailBox.flag)
+	{
+
+	}
+	UART0_MailBox.flag = FALSE;
 }
