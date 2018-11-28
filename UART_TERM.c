a810a511dd0eabe44c56804227fc7803aa43858b
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-28 01:47:24 -0600

    Add files via upload

--- UART_TERM.c
+++ UART_TERM.c
@@ -23,3 +23,27 @@ void TERA_master()
 	/** VT100 command for positioning the cursor in x and y position*/
 	UART_putString(UART_0, "\033[11;10H");
 }
+void TERA_choose()
+{
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0, "\033[12;10H");
+	/** Set the text in a string*/
+	UART_putString(UART_0, "Chose either to add user or open safe\r");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0, "\033[12;10H");
+	/** Set the text in a string*/
+	UART_putString(UART_0, "1) Open safe\r");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0, "\033[13;10H");
+	/** Set the text in a string*/
+	UART_putString(UART_0, "2)Add user\r");
+}
+void TERA_wrong()
+{
+	/*VT100 command for clearing the screen*/
+	UART_putString(UART_0,"\033[2J");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0, "\033[10;10H");
+	/** Set the text in a string*/
+	UART_putString(UART_0, "Put a correct number\r");
+}
--- UART_capture.c
+++ UART_capture.c
@@ -10,6 +10,12 @@
 /**This is mail box to received the information from the serial port*/
 extern UART_MailBoxType UART0_MailBox;
 
+/*Flags so we can change to the next state*/
+static uint8 flag_master = FALSE;
+static uint8 flag_wrong = FALSE;
+static uint8 flag_open = FALSE;
+static uint8 flag_add = FALSE;
+
 void capture_psw()
 {
 	sint8 psw_buffer[PSW_SIZE] = FALSE;
@@ -20,7 +26,38 @@ void capture_psw()
 			*psw_buffer = UART0_MailBox.mailBox;
 			UART_putChar(UART_0,*psw_buffer);
 			*psw_buffer++;
+			if(psw_buffer[PSW_SIZE] == NULL)
+			{
+				flag_master = TRUE;
+			}
 		}
 	}
 	UART0_MailBox.flag = FALSE;
+}
+
+void choose(uint8 flag_master)
+{
+	TERA_choose();
+	if(flag_master == TRUE)
+	{
+		if (UART0_MailBox.flag)
+		{
+			if((ASCII_NUMBER_1 == UART0_MailBox.mailBox) || (ASCII_NUMBER_2 == UART0_MailBox.mailBox))
+			{
+				if(ASCII_NUMBER_1 == UART0_MailBox.mailBox)
+				{
+					flag_open = TRUE;
+				}
+				if(ASCII_NUMBER_2 == UART0_MailBox.mailBox)
+				{
+					flag_add = TRUE;
+				}
+			}
+			else
+			{
+				flag_wrong = TRUE;
+			}
+		}
+		UART0_MailBox.flag = FALSE;
+	}
 }
