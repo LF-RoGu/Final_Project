7e43db8745e0890f3b70ebf35cd0b403e929bf1c
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-28 03:37:08 -0600

    Add files via upload

--- UART_capture.c
+++ UART_capture.c
@@ -36,6 +36,7 @@ void capture_psw()
 
 void choose(uint8 flag_master)
 {
+	volatile uint8 choice = FALSE;
 	TERA_choose();
 	if(flag_master == TRUE)
 	{
@@ -50,10 +51,13 @@ void choose(uint8 flag_master)
 				if(ASCII_NUMBER_2 == UART0_MailBox.mailBox)
 				{
 					flags.flag_add = TRUE;
+					flags.flag_code = TRUE;
 				}
+				choice = UART0_MailBox.mailBox;
 			}
 			else
 			{
+				flags.flag_error = TRUE;
 				flags.flag_wrong = TRUE;
 			}
 		}
@@ -131,9 +135,78 @@ void add_user()
 
 void add_code()
 {
-	if (UART0_MailBox.flag)
-	{
+	volatile uint8 index_code = FALSE;
 
+	while(flags.flag_code)
+	{
+		if (UART0_MailBox.flag)
+		{
+			psw_buffer[index_code] = (UART0_MailBox.mailBox - ASCII_DECODE);
+			index_code++;
+			if(index_code == PSW_SIZE)
+			{
+				index_code = FALSE;
+				flags.flag_code = FALSE;
+				flags.flag_error = TRUE;
+				counter.codes++;
+			}
+			if(ASCII_ENTER == UART0_MailBox.mailBox)
+			{
+				TERA_wrong();
+				flags.flag_code = FALSE;
+				flags.flag_error = TRUE;
+			}
+		}
+		UART0_MailBox.flag = FALSE;
 	}
-	UART0_MailBox.flag = FALSE;
+	/*Clears index to store it in the memory*/
+	index_user = FALSE;
+	switch(counter.codes)
+	{
+	case BIT1:
+		for(uint8 arr_index = USER_CODE; arr_index < (USER_CODE + MEM_STEP); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT2:
+		for(uint8 arr_index = (USER_CODE + MEM_STEP); arr_index < (USER_CODE + (BIT2 * MEM_STEP)); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT3:
+		for(uint8 arr_index = (USER_CODE + (BIT2 * MEM_STEP)); arr_index < (USER_CODE + (BIT3 * MEM_STEP)); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT4:
+		for(uint8 arr_index = (USER_CODE + (BIT3 * MEM_STEP)); arr_index < (USER_CODE + (BIT4 * MEM_STEP)); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	case BIT5:
+		for(uint8 arr_index = (USER_CODE + (BIT4 * MEM_STEP)); arr_index < (USER_CODE + (BIT5 * MEM_STEP)); arr_index++)
+		{
+			EEPROM_write_mem(arr_index,user_buffer[index_user]);
+			index_user++;
+		}
+		/*Clears index to store it in the memory*/
+		index_user = FALSE;
+		break;
+	}
 }
